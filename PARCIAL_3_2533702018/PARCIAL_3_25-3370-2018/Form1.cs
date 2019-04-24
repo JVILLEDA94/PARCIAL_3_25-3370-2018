eb3b01a151baaee38c0aff6584506a8489a2f69c
JVILLEDA94 <47669945+JVILLEDA94@users.noreply.github.com>
2019-04-23 19:58:58 -0600

    Add files via upload

---
+++ PARCIAL_3_2533702018/PARCIAL_3_25-3370-2018/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PARCIAL_3_25_3370_2018
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario, password;
+            usuario = textBox1.Text.TrimEnd();
+            password = textBox2.Text.TrimEnd();
+            if ((usuario == "JVILLEDA") && (password == "1234567"))
+            {
+
+                MessageBox.Show("Bienvenidos al sistema");
+
+                MDIParent1 obj = new MDIParent1();
+
+                obj.Show();
+
+                
+
+            }
+            else
+            {
+                MessageBox.Show("Verifique usuario y password");
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
---
+++ PARCIAL_3_2533702018/PARCIAL_3_25-3370-2018/Form3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PARCIAL_3_25_3370_2018
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string Nombre, Carnet, Materia, Ciclo;
+
+            Nombre = textBox1.Text.TrimEnd();
+            Carnet = textBox2.Text.TrimEnd();
+            Materia = textBox3.Text.TrimEnd();
+            Ciclo = textBox4.Text.TrimEnd();
+
+            if ((Nombre == "") || (Carnet == "") || (Materia == "") || (Ciclo == ""))
+            {
+
+
+                MessageBox.Show("todos los campos son obligatorios");
+
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+
+                Form3 obj = new Form3();
+
+                obj.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Datos ingresados");
+
+                MDIParent1 obj = new MDIParent1();
+
+                obj.Show();
+            }
+        }
+    }
+}
